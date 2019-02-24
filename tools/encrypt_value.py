b100303c821ffe352adfef50027f7f6e2748ed73
brianddk <brianddk@users.noreply.github.com>
2019-02-24 15:00:38 -0600

    flake8 compliance

--- tools/encrypt_value.py
+++ tools/encrypt_value.py
@@ -2,33 +2,37 @@
 #
 # Note, this doesn't use the same address (-n) as Trezor Password Manager
 #   By default it will store the address and key in clear text and only
-#   encrypt the value.  Use --secure to force the user to name key and 
-#   address on the command line, and keep it out of the armor file. 
+#   encrypt the value.  Use --secure to force the user to name key and
+#   address on the command line, and keep it out of the armor file.
 
 from trezorlib import misc, tools, ui
 from trezorlib import __version__ as trezor_version
 from trezorlib.client import TrezorClient
 from trezorlib.transport import get_transport
 from random import getrandbits
-import fileinput, sys, base64, argparse
+import sys
+import base64
+import argparse
+
 
 def key_to_addr(key):
     addr = []
     try:
         key_hex = bytes.fromhex(key).hex()
-    except:
+    except ValueError:
         key_hex = str.encode(key).hex()
     while len(key_hex):
         addr.append(int(key_hex[:8], 16))
         key_hex = key_hex[8:]
     return addr
-        
+
+
 def input_key_addr(value, kheader, aheader, args):
-    key = getrandbits(key_len*8).to_bytes(key_len, byteorder='big').hex()
+    key = getrandbits(key_len * 8).to_bytes(key_len, byteorder='big').hex()
     addr = False
     if args.decrypt:
         for line in value.decode().split('\n'):
-            line = line.replace('\r','')
+            line = line.replace('\r', '')
             words = line.split()
             if words and words[0] == kheader:
                 x = len(kheader) + 1
@@ -46,38 +50,44 @@ def input_key_addr(value, kheader, aheader, args):
         addr = key_to_addr(key)
     return key, addr
 
+
 def pad_value(value):
     p = 16 - len(value) % 16
     for i in range(0, p):
         value += int(p).to_bytes(1, byteorder='big')
     return value
-    
+
+
 def depad_value(value):
     p = 0 - value[-1]
-    return value[:p]    
-    
+    return value[:p]
+
+
 def input_value(args):
     value = sys.stdin.buffer.read()
     if not args.decrypt:
         value = pad_value(value)
     return value
 
+
 def print_enarmor(binstr):
     astr = base64.standard_b64encode(enc).decode()
     while astr:
         print(astr[:64])
         astr = astr[64:]
-    
+
+
 def dearmor(value):
     dec = ''
     for line in value.decode().split('\n'):
-        line = line.replace('\r','').replace(' ','')
+        line = line.replace('\r', '').replace(' ', '')
         if len(line.split(':')) > 1 or len(line.split('-')) > 1:
             pass
         else:
-            dec += line.replace('\r','')
+            dec += line.replace('\r', '')
     return base64.standard_b64decode(dec)
-    
+
+
 def encode_addr(addr):
     eaddr = []
     for i in addr:
@@ -85,8 +95,9 @@ def encode_addr(addr):
             i -= 0x80000000
             i = str(i) + "'"
         eaddr.append(i)
-    return "m/" + "/".join(list(map(str,eaddr)))
-    
+    return "m/" + "/".join(list(map(str, eaddr)))
+
+
 if __name__ == '__main__':
     # Data
     version  = 0.2
@@ -103,7 +114,7 @@ if __name__ == '__main__':
     parser.add_argument("-v", "--verbose", action="store_true", help="high verbosity")
     args = parser.parse_args()
     device = get_transport()
-    client = TrezorClient(transport=device, ui=ui.ClickUI())    
+    client = TrezorClient(transport=device, ui=ui.ClickUI())
     value = input_value(args)
     key, addr = input_key_addr(value, kheader, aheader, args)
     if args.verbose:
@@ -111,7 +122,7 @@ if __name__ == '__main__':
     if args.decrypt:
         dec = misc.decrypt_keyvalue(client, addr, key, dearmor(value), ask_on_encrypt=False, ask_on_decrypt=False)
         sys.stdout.buffer.write(depad_value(dec))
-    else:    
+    else:
         if len(value) > 1024:
             print("ERROR: Buffer exceeded max length of 1024")
             exit(-1)
--- tools/wallet_search.py
+++ tools/wallet_search.py
@@ -9,8 +9,13 @@ from trezorlib.transport import get_transport
 from trezorlib.tools import parse_path
 from trezorlib import btc, ethereum, messages
 from trezorlib.ui import ClickUI
+from trezorlib.exceptions import TrezorFailure
 from os.path import isfile
-import json, glob, urllib.request
+from urllib.error import HTTPError
+import urllib.request
+import json
+import glob
+
 
 def get_ethereum_address(path):
     transport = get_transport()
@@ -19,31 +24,31 @@ def get_ethereum_address(path):
     try:
         client = TrezorClient(transport, ui)
         address = ethereum.get_address(client, parse_path(path))
-    except:
-        pass
     finally:
         client.close()
     return '0x' + address.hex()
-    
+
+
 def get_address(coin, path, addr_type):
     if addr_type == 'address':
-        script_type=messages.InputScriptType.SPENDADDRESS
+        script_type = messages.InputScriptType.SPENDADDRESS
     if addr_type == 'p2shsegwit':
-        script_type=messages.InputScriptType.SPENDP2SHWITNESS
+        script_type = messages.InputScriptType.SPENDP2SHWITNESS
     if addr_type == 'segwit':
-        script_type=messages.InputScriptType.SPENDWITNESS
+        script_type = messages.InputScriptType.SPENDWITNESS
     transport = get_transport()
     ui = ClickUI()
     address = 'Error'
     try:
         client = TrezorClient(transport, ui)
         address = btc.get_address(client, coin['coin_name'], parse_path(path), show_display=False, multisig=None, script_type=script_type)
-    except:
+    except TrezorFailure:
         pass
     finally:
         client.close()
     return address
-    
+
+
 def get_path(coin, addr_type, account, change, index):
     if addr_type == 'address':
         path = "m/44'/" + str(coin['slip44']) + "'/" + str(account) + "'/" + str(change) + "/" + str(index)
@@ -52,6 +57,7 @@ def get_path(coin, addr_type, account, change, index):
     if addr_type == 'segwit':
         path = "m/84'/" + str(coin['slip44']) + "'/" + str(account) + "'/" + str(change) + "/" + str(index)
     return path
+
 
 def get_coins():
     coins = []
@@ -63,7 +69,7 @@ def get_coins():
     # BCH-SV might be added here.
     # for filename in glob.glob('custom/*.json'):
         # files.append(filename.replace('\\', '/'))
-        
+
     for file in files:
         if not isfile(file):
             print("ERROR:", file, "not found")
@@ -82,15 +88,16 @@ def get_coins():
                 if 'name' in item:
                     item['coin_name'] = item['name']
                     item['coin_label'] = item['name']
-                    
+
                 # Use blocks like this to filter out known "alpha" coins
                 # if item['coin_name'] == 'PIVX':
                     # continue
-                                        
-                if not 'segwit' in item:
+
+                if 'segwit' not in item:
                     item['segwit'] = False
                 coins.append(item)
     return coins
+
 
 def verify_coins(coins):
     verified = []
@@ -103,12 +110,12 @@ def verify_coins(coins):
             try:
                 with urllib.request.urlopen(q) as req:
                     data = req.read().decode()
-                    if(data.replace(' ','').find('"inSync":true') >= 0):
+                    if(data.replace(' ', '').find('"inSync":true') >= 0):
                         coin['api'] = url
                         break
-            except:
+            except HTTPError:
                 pass
-                
+
         if 'coin_label' in coin:
             label = coin['coin_label']
         else:
@@ -120,17 +127,19 @@ def verify_coins(coins):
             print("FAILED:", label)
 
     return verified
-    
-def get_address_info(coin, address):   
+
+
+def get_address_info(coin, address):
     url = coin['api'] + 'address/' + address
     q = urllib.request.Request(url)
     q.add_header('User-Agent', 'curl/7.55.1')
     try:
         with urllib.request.urlopen(q) as req:
             data = json.loads(req.read().decode())
-    except:
+    except HTTPError:
         raise
     return data
+
 
 def main():
     coins = get_coins()
@@ -144,8 +153,8 @@ def main():
         account = -1
         blank_accounts = 0
         while(blank_accounts < max_accounts):
-            account +=1
-            blank_accounts +=1
+            account += 1
+            blank_accounts += 1
             if coin['segwit']:
                 types = ['address', 'p2shsegwit', 'segwit']
             else:
@@ -160,21 +169,21 @@ def main():
                         fmt_type = addr_type
                         # for fmt_type in types: # optional extra loop for fmt_type addr_type errors.
                         path = get_path(coin, addr_type, account, change, index)
-                        #[debug]# print(blank_accounts, blank_addresses, path, fmt_type)
+                        # print(blank_accounts, blank_addresses, path, fmt_type)
                         if 'chain_id' in coin:
                             address = get_ethereum_address(path)
                         else:
                             address = get_address(coin, path, fmt_type)
                         try:
                             addr_info = get_address_info(coin, address)
-                        except:
+                        except HTTPError:
                             print("ERROR:", coin['coin_label'], path, address)
-                            addr_info = { 'txApperances': 0 }
+                            addr_info = {'txApperances': 0}
                         if (('txApperances' in addr_info) and (addr_info['txApperances'] > 0)) or \
                                 (('txs' in addr_info) and (addr_info['txs'] > 0)):
                             blank_accounts = blank_addresses = 0
                             print("ACTIVE:", coin['coin_label'], path, address)
 
+
 if __name__ == '__main__':
     main()
-
